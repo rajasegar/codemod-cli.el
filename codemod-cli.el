ec89da85957b5cfd6c87c46745b0e3102d6c8553
Rajasegar Chandran <rajasegar.c@gmail.com>
2024-02-08 15:20:06 +0530

    chore: select codemods when creating fixtures

--- codemod-cli.el
+++ codemod-cli.el
@@ -19,9 +19,27 @@
 (defun codemod-cli-generate-fixture ()
   "Generate fixture for a codemod"
   (interactive)
-  (let ((codemod-name (read-string "Codemod name: "))
-        (fixture-name (read-string "Fixture name: ")))
-    (async-shell-command (format "npx codemod-cli generate fixture %s %s" codemod-name fixture-name))))
+  (if (> (length (get-codemods)) 0)
+        (ivy-read "Select a Codemod: "
+                  (lambda (str pred _)
+                    (let* ((props (get-codemods))
+                           (strs (get-codemods)))
+                      (cl-mapcar (lambda (s p) (propertize s 'property p)) strs props)))
+                  :action (lambda (x)
+                            (let ((codemod-name (get-text-property 0 'property x))
+                                  (fixture-name (read-string "Fixture name: "))
+                                  (default-directory (get-project-root)))
+                              (async-shell-command (format "npx codemod-cli generate fixture %s %s" codemod-name fixture-name)))))
+      (if 
+          (y-or-n-p "You haven't created any codemods, do you want to create one? ")
+          (let ((codemod-name (read-string "Codemod name: ")))
+            (if (shell-command (format "npx codemod-cli generate codemod %s" codemod-name))
+                (let ((fixture-name (read-string "Fixture name: ")))
+                  (async-shell-command (format "npx codemod-cli generate fixture %s %s" codemod-name fixture-name)))
+              (message "Codemod creation failed. please try again")))
+        (message "Please create a codemod first and then generate fixtures"))
+      
+      ))
 
 (defun codemod-cli-update-docs ()
   "Update the codemod docs"
@@ -31,8 +49,20 @@
 (defun get-codemods ()
 (remove-if (lambda (x)
              (or (string= x ".") (string= x "..") (string= x ".gitkeep")))
-           (directory-files "~/www/react-codemod/transforms")))
+           (directory-files (concat (get-project-root) "/transforms"))))
+
+(defun get-project-root ()
+  "Find the project root."
+    (locate-dominating-file (file-name-directory (buffer-file-name)) "package.json"))
+
+
+
+
+
+(get-codemods)
+
 
 (codemod-cli-new-project)
+
 
 
